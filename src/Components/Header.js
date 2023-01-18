6fc9dca1471403a896d9fc79b49bceae8fe20508
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-19 01:30:16 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./Headers.css";
+import headerLogp from '../Image/HeaderLogo.png';
 
 export default function Header() {
   return (
     <div className='headerBody'>
-
+        <div>
+            <img src='headerLogp' />
+        </div>
     </div>
   )
 }
