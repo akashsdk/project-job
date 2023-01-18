4e746e71056c6946100c3b918da26120e2253826
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-18 18:49:51 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import Home from "./Screen/Home";
 import Notfound from "./Screen/Notfound";
-import 'antd/dist/reset.css';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import "antd/dist/reset.css";
 
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="*" element={<Notfound NotfoundText="Page Not Found" />} />
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
