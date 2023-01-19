c82e1864d094dd19680578ac83897f7d351b8245
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-19 18:08:11 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import "../Styles/Home.css";
 
 export default function Home() {
   return (
-    <div className='homeBody'>Homeok</div>
-  )
+    <div className="homeBody">
+        <div style={{height:'25px'}}></div>
+      <div className="homeBox">dddd</div>
+    </div>
+  );
 }
