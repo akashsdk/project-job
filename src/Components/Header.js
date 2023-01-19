9e41a74f07fa4fffe387fb1e946975f6e1031573
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-19 14:58:23 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,66 @@
-import React from "react";
+import React,{ useState } from "react";
 import "./Headers.css";
 import headerLogp from "../Image/HeaderLogo.png";
 
 export default function Header() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
     <div className="headerBody">
       <div className="headerBox">
         <img src={headerLogp} className="headerImg" />
-        <h1 style={{
-            color:'white',
-            marginLeft:'10px',
-        }}>Idea</h1>
+        <h1
+          style={{
+            color: "white",
+            marginLeft: "10px",
+          }}
+        >
+          Idea
+        </h1>
         <div>
-            
+          <div>
+            <nav className="navigation">
+              <a href="/" className="brand-name">
+                TasnimShahriarAkash
+              </a>
+              <button
+                className="hamburger"
+                onClick={() => {
+                  setIsNavExpanded(!isNavExpanded);
+                }}
+              >
+                {/* icon from Heroicons.com */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="white"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+              <div
+                className={
+                  isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+                }
+              >
+                <ul>
+                  <li>
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <a href="/about">About</a>
+                  </li>
+                  <li>
+                    <a href="/contact">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
