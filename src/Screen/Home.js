6e1ff79e2f3b79fcfb3cd4dfed310ddca3c23fcd
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-21 00:37:08 +0600

    Update Home.js

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -16,6 +16,84 @@ akas
             </div>
         </div>
       </div>
+      https://tasnim-shahriar-akash.web.app/
+      <div style={{ height: "25px" }}></div>
+      <div className="homeBox">
+        <div style={{ height: "20px"}}></div>
+        <div style={{ marginLeft:'20px'}}>
+        <h1 style={{color:'#562EFF'}}>Top Player</h1>
+            <div>
+                <div>
+akas
+                </div>
+            </div>
+        </div>
+      </div>
+      https://tasnim-shahriar-akash.web.app/
+      <div style={{ height: "25px" }}></div>
+      <div className="homeBox">
+        <div style={{ height: "20px"}}></div>
+        <div style={{ marginLeft:'20px'}}>
+        <h1 style={{color:'#562EFF'}}>Top Player</h1>
+            <div>
+                <div>
+akas
+                </div>
+            </div>
+        </div>
+      </div>
+      https://tasnim-shahriar-akash.web.app/
+      <div style={{ height: "25px" }}></div>
+      <div className="homeBox">
+        <div style={{ height: "20px"}}></div>
+        <div style={{ marginLeft:'20px'}}>
+        <h1 style={{color:'#562EFF'}}>Top Player</h1>
+            <div>
+                <div>
+akas
+                </div>
+            </div>
+        </div>
+      </div>
+      https://tasnim-shahriar-akash.web.app/
+      <div style={{ height: "25px" }}></div>
+      <div className="homeBox">
+        <div style={{ height: "20px"}}></div>
+        <div style={{ marginLeft:'20px'}}>
+        <h1 style={{color:'#562EFF'}}>Top Player</h1>
+            <div>
+                <div>
+akas
+                </div>
+            </div>
+        </div>
+      </div>
+      https://tasnim-shahriar-akash.web.app/
+      <div style={{ height: "25px" }}></div>
+      <div className="homeBox">
+        <div style={{ height: "20px"}}></div>
+        <div style={{ marginLeft:'20px'}}>
+        <h1 style={{color:'#562EFF'}}>Top Player</h1>
+            <div>
+                <div>
+akas
+                </div>
+            </div>
+        </div>
+      </div>
+      https://tasnim-shahriar-akash.web.app/
+      <div style={{ height: "25px" }}></div>
+      <div className="homeBox">
+        <div style={{ height: "20px"}}></div>
+        <div style={{ marginLeft:'20px'}}>
+        <h1 style={{color:'#562EFF'}}>Top Player</h1>
+            <div>
+                <div>
+akas
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
