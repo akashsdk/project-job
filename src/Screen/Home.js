d16b70bcbf4e1cdc4238c43336f1818da7434d50
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-21 00:34:45 +0600

    Update Home.js

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -4,7 +4,18 @@ import "../Styles/Home.css";
 export default function Home() {
   return (
     <div className="homeBody">
-     
+      <div style={{ height: "25px" }}></div>
+      <div className="homeBox">
+        <div style={{ height: "20px"}}></div>
+        <div style={{ marginLeft:'20px'}}>
+        <h1 style={{color:'#562EFF'}}>Top Player</h1>
+            <div>
+                <div>
+akas
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
